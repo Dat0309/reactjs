54504026a3f7837ea79b97ced70a18d9666ad3c9
Dat0309 <anhtheboy011@gmail.com>
2021-12-08 13:05:24 +0700

    Movie app

---
+++ movie-watching/src/config/Routes.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import {Route, Switch } from 'react-router-dom';
+
+import Home from '../pages/Home';
+import Catalog from '../pages/Catalog';
+import Detail from '../pages/Detail';
+
+export default function Routes() {
+    return (
+        <Switch>
+            <Route
+                path='/:category/search/:keyword'
+                component={Catalog}
+            />
+            <Route
+                path='/:category/:id'
+                component={Detail}
+            />
+            <Route
+                path='/:category'
+                component={Catalog}
+            />
+            <Route
+                path='/'
+                exact
+                component={Home}
+            />
+        </Switch>
+    )
+}
---
+++ movie-watching/src/pages/Catalog.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Catalog() {
+    return (
+        <div>
+            Catalog
+        </div>
+    )
+}
